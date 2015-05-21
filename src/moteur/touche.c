5acab00bb72e3ce676f5af9188c9fee0ae3e72db
Olivier-Daire <daire.olivier@gmail.com>
2015-05-21 17:35:34 +0200

    Remove useless code

--- src/moteur/touche.c
+++ src/moteur/touche.c
@@ -9,14 +9,10 @@ void appuyer(Personnage *perso, SDL_Event e){
     if(e.type == SDL_KEYDOWN){
         switch(e.key.keysym.sym){
             case SDLK_RIGHT :
-                perso->gauche = false;
-                perso->haut = false;
                 perso->droite = true;
             break; 
 
             case SDLK_LEFT : 
-                perso->droite = false;
-                perso->haut = false;
                 perso->gauche = true;
             break;
 
@@ -48,37 +44,21 @@ void relacher(Personnage *perso, SDL_Event e){
 
         switch(e.key.keysym.sym){
             case SDLK_RIGHT :
-                perso->gauche = false;
                 perso->droite = false;
-                // Fix pour si les deux touches sont appuyées en même temps
-                // l'un des deux à tendance à rester appuyée
-                
             break; 
 
             case SDLK_LEFT : 
-                perso->droite = false;
                 perso->gauche = false;
-                // Fix pour si les deux touches sont appuyées en même temps
-                // l'un des deux à tendance à rester appuyée
-                
             break;
 
             case SDLK_DOWN : 
                 perso->bas = false;
-                // Fix pour si les deux touches sont appuyées en même temps
-                // l'un des deux à tendance à rester appuyée
-                //perso->droite = false;
-                //perso->gauche = false;
             break;
 
             case SDLK_UP : 
             case SDLK_SPACE :
                 perso->haut = false;
                 perso->gravite = 10;
-                // Fix pour si les deux touches sont appuyées en même temps
-                // l'un des deux à tendance à rester appuyée
-                //perso->droite = false;
-                //perso->gauche = false;   // desactivé sinon mouvements pas fluides
             break;
 
             default : break;
