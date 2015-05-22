1751f60fd7c5802447593a09771ce58aa0aad935
Romain François <belum76@gmail.com>
2015-05-23 00:58:58 +0200

    COLLISIONS [ALL] DONE

--- src/moteur/colision.c
+++ src/moteur/colision.c
@@ -6,33 +6,51 @@
 
 /**
  * Parcours les cases devant et derière selon la largeur du solide et teste les collisions
- * FIXME : bug avec des perso ayant une hauteur impaire : il se place bien mais ne veut plus sauter à moins de définir une gravité énorme à chaque saut genre -25 dans touche.c
+ * 
  * 
  * @param *perso
  */
  bool collisionGround(Personnage *perso, int** level){
-    int i = 0;
-	// colonne (grille) du perso, concernée par la colision (sens)
-    //int column = convertPixelToCase(perso->centerX + perso->vitesse * perso->sens);
-	//ligne (grille) du point bas du perso, on considère que ses pieds sont à 1 pixel du bord de la case
-    int line = convertPixelToCase(perso->centerY + (perso->height/2.0));
 
-   int halfWidth = (perso->width/2.0);
-   int halfHeight = (int)ceil(perso->height/2.0);
 
-        for (i = floor((perso->centerX)/TAILLE_CASE - halfWidth); i <= ceil((perso->centerX)/TAILLE_CASE + halfWidth); i++)
-        {
-            
-           if (level[line + halfHeight][i] == 1)
-           {
+    int Y = perso->centerY;
+    int X = perso->centerX;
+
+    float halfHeight = perso->height/2.0;
+    float halfWidth = perso->width/2.0;
+
+    int C = (X - (halfWidth * TAILLE_CASE)) / TAILLE_CASE;
+    int L = (Y + (halfHeight * TAILLE_CASE)) / TAILLE_CASE;
+
+
+    for ( ; C <= floor((X + (halfWidth * TAILLE_CASE)) / TAILLE_CASE)  ; C++){
+
+        if(level[L][C] == 1 &&  Y >=(L - halfHeight)*TAILLE_CASE){
+
+            switch(perso->height){                                                      // SWITCH pour corriger la position selon la hauteur du perso (1-6)
+                case 1:
+                case 2:
+                    perso->centerY = (L - halfHeight)*TAILLE_CASE - halfHeight +1 ;
+                    break;
+                case 3:
+                case 4:
+                    perso->centerY = (L - halfHeight)*TAILLE_CASE - halfHeight +2 ;
+                    break;
+                case 5:
+                case 6:
+                    perso->centerY = (L - halfHeight)*TAILLE_CASE - halfHeight +3 ;
+                    break;
+
+                default:
+                    perso->centerY = (L - halfHeight)*TAILLE_CASE - halfHeight +1 ;
+                    break;
+            }
+
             perso->gravite = 10;
-            perso->centerY = convertCaseToPixel(line + perso->height/2.0) - convertCaseToPixel(perso->height/2.0);
             perso->saute = false;
             return true;
-
-            }
         }
-
+    }
     return false;
 }
 
@@ -47,7 +65,7 @@ int collisionLateral(Personnage *perso, int** level){
     int C = floor((X + (halfWidth*TAILLE_CASE) * perso->sens) / TAILLE_CASE);    
     int L = floor((Y - (halfHeight*TAILLE_CASE)) / TAILLE_CASE);
  
-    // latéral                          
+                         
     for (; L < (Y/TAILLE_CASE) +floor(halfHeight); L++) {
         if (level[L][C]==1 && perso->sens == 1) {
             perso->centerX = C * TAILLE_CASE + floor(halfWidth) - halfWidth*TAILLE_CASE * perso->sens -2;
@@ -55,7 +73,7 @@ int collisionLateral(Personnage *perso, int** level){
         else if (level[L][C]==1 && perso->sens == -1) {
             if(perso->width %2==0) perso->centerX = C * TAILLE_CASE - floor(halfWidth) - perso->width*TAILLE_CASE * perso->sens +1;
             else {
-               perso->centerX = C * TAILLE_CASE - floor(halfWidth) - (perso->width/2.0)*TAILLE_CASE * perso->sens +1*TAILLE_CASE +2;
+               perso->centerX = C * TAILLE_CASE - floor(halfWidth) - (perso->width/2.0)*TAILLE_CASE * perso->sens +1*TAILLE_CASE +1 ;
             }                 
         }
     }
--- src/moteur/deplacements.c
+++ src/moteur/deplacements.c
@@ -9,20 +9,14 @@
 void deplacementJoueur(Personnage *perso, int** level){
     perso->centerX += perso->vitesse * perso->sens;
     collisionLateral(perso, level);
+   
+ 	perso->centerY += perso->gravite++;
 
- perso->centerY += perso->gravite++;
-//     // Collision haut
     if(CollisionRoof(perso, level) == 1){
          perso->saute = false;
          perso->gravite = 10;
         perso->centerY += perso->gravite++;
     }
-
-//     // Collision bas
-//     if(testCollisonGround(perso, level) == -1){
-//          return;
-//     }    
-    // if(perso->gravite < 0 && collisionTop(perso, level)) return;
     if(collisionGround(perso, level)) return;
     
 }
