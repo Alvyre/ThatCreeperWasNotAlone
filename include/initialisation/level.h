019fdce3b35e22b00ea4494e840608de0f06199f
Olivier-Daire <daire.olivier@gmail.com>
2015-05-27 22:31:51 +0200

    Cleanup

--- include/initialisation/level.h
+++ include/initialisation/level.h
@@ -1,10 +1,6 @@
 #ifndef LEVEL_H
 #define LEVEL_H
-#include <stdbool.h> //FIXME
-#include <stdio.h> // FIXME
-#include <stdlib.h> //FIXME
 #include "moteur/main.h"
-
 
 typedef struct Level {
  int width;
@@ -15,7 +11,6 @@ typedef struct Level {
 Level* initLevel(Level* level, int width, int height);
 void freeLevel(Level* level);
 void creeDecor(Level* level);
-bool isPixelGround(int pixelX, int pixelY, int **level);
 Level* loadLevelFromFile(char const * path, int persoInfos[3][8], int *nbrPerso);
 char const *selectLevelFromNumber(int levelNumber);
 
