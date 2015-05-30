30fc0e60350473af4c8bc67605b70ef3c6b5142d
Olivier-Daire <daire.olivier@gmail.com>
2015-05-30 22:55:22 +0200

    Camera

--- src/moteur/scrolling.c
+++ src/moteur/scrolling.c
@@ -12,43 +12,38 @@ void testCam(Personnage *perso, Camera *camera, Level* level){
 	// MAx position caméra = level->width*TAILLE_CASE - WINDOW_WIDTH/2
 	// Min position caméra = 0
 
-		// Perso va vers la gauche
-		if (perso->gauche && camera->currentX - WINDOW_WIDTH/2 >= 0)
-		{
-		// 	if (camera->currentX - perso->box.pos.x > WINDOW_WIDTH/2)
-		// {
-			glTranslatef(perso->vitesse,0,0);
-			camera->currentX -= perso->vitesse;	
+
+	// Perso va vers la gauche
+	if (perso->gauche && camera->currentX - WINDOW_WIDTH/2 >= 0)
+	{
+		glTranslatef(perso->vitesse,0,0);
+		camera->currentX -= perso->vitesse;	
 			
-			//} 
-		}
+	}
 
-		// Perso va vers la droite
-		if (perso->droite && camera->currentX < level->width*TAILLE_CASE - WINDOW_WIDTH/2)
-		{
-			glTranslatef(-perso->vitesse,0,0);
-			camera->currentX = camera->currentX +perso->vitesse;
-		}
+	// Perso va vers la droite
+	if (perso->droite && camera->currentX < level->width*TAILLE_CASE - WINDOW_WIDTH/2)
+	{
+		glTranslatef(-perso->vitesse,0,0);
+		camera->currentX = camera->currentX +perso->vitesse;
+	}
 
-		// Si perso trop en arrière par rapport à la caméra
-		// FIXME : fait trembler la caméra
-		if (camera->currentX - perso->box.pos.x > WINDOW_WIDTH/4)
-		{
-			while(camera->currentX - perso->box.pos.x > WINDOW_WIDTH/4 && camera->currentX - WINDOW_WIDTH/2 >= 0){
-				glTranslatef(10,0,0);
-				camera->currentX -= 10;	
-			}
-			printf("Trop loinf\n");
+	// Si perso trop en arrière par rapport à la caméra
+	if (camera->currentX - perso->box.pos.x > WINDOW_WIDTH/4)
+	{
+		while(camera->currentX - perso->box.pos.x > WINDOW_WIDTH/4 && camera->currentX - WINDOW_WIDTH/2 >= 0){
+			glTranslatef(1,0,0);
+			camera->currentX -= 1;	
 		}
+	}
 
-		// si perso trop en avance sur la caméra
-		if (perso->box.pos.x - camera->currentX > WINDOW_WIDTH/4)
-		{
-			while(perso->box.pos.x - camera->currentX > WINDOW_WIDTH/4 && camera->currentX + WINDOW_WIDTH/2 <= level->width*TAILLE_CASE){
-				glTranslatef(-10,0,0);
-				camera->currentX += 10;	
-			}
-			printf("Trop loinf après\n");
+	// si perso trop en avance sur la caméra
+	if (perso->box.pos.x - camera->currentX > WINDOW_WIDTH/4)
+	{
+		while(perso->box.pos.x - camera->currentX > WINDOW_WIDTH/4 && camera->currentX + WINDOW_WIDTH/2 <= level->width*TAILLE_CASE){
+			glTranslatef(-1,0,0);
+			camera->currentX += 1;	
 		}
+	}
 
 }
